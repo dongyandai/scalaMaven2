1a2213fa6dc8e74e038239553934510b8071e9ad
dongyandai <dongyandai@gmail.com>
2023-01-23 12:10:21 -0500

    some changes

--- src/main/scala/BD_git.scala
+++ src/main/scala/BD_git.scala
@@ -1,1 +1,13 @@
+package com.statistics
 
+import com.statistics.bean.{GenresRecommendation, MongoConfig, Movie, Rating, Recommendation}
+
+import java.text.SimpleDateFormat
+import java.util.Date
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.{DataFrame, SparkSession}
+
+
+object StatisticsRecommender {
+
+// 定义表名
